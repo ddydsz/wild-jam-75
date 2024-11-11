e5c801e7a0cdda306e71ade2dc94c5de5e077f2e
dsz <dsz@email.com>
2024-11-11 16:35:36 -0500

    smoothed echo pulse

--- EcholocationDemo/world.gd
+++ EcholocationDemo/world.gd
@@ -8,7 +8,7 @@ var pulse_min_distance = 0.0;
 
 const pulse_width = 0.15;
 const pulse_max_range = 15.0;
-const pulse_speed = 10.0;
+const pulse_speed = 15.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,5 +20,4 @@ func _process(delta):
 	if pulse_min_distance > pulse_max_range:
 		pulse_min_distance = 0
 		
-	echo_material.set_shader_parameter("min_distance", pulse_min_distance)
-	echo_material.set_shader_parameter("max_distance", pulse_min_distance + pulse_width)
+	echo_material.set_shader_parameter("pulse_distance", pulse_min_distance)
