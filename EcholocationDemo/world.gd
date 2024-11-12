629c8065ba72bc2ba5390c306c90b18531a932a3
dsz <dsz@email.com>
2024-11-11 20:43:21 -0500

    add cave

--- EcholocationDemo/world.gd
+++ EcholocationDemo/world.gd
@@ -12,7 +12,8 @@ const pulse_speed = 15.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	echo_material = sphere.mesh.material;
+	echo_material = load("res://EcholocationDemo/echo_material.tres")
+	#echo_material = sphere.mesh.material;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
