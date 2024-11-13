d32990c6cf59a4470bca4b336ec802bdf1637354
Raman Khakh <ramanjit456@yahoo.co.in>
2024-11-12 20:15:03 -0500

    added spider

--- EcholocationDemo/MainCamera.gd
+++ EcholocationDemo/MainCamera.gd
@@ -40,10 +40,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	process_basic_input()
 	
-	
 	# bat waving in place
 	self.translate(Vector3(0, sin(Time.get_ticks_msec()/SIN_WAVE_TIME_DIVISION)/SIN_WAVE_AMPLITUDE_DIVISION, 0))
-	
 	
 	if Input.is_action_pressed("ui_up"):
 		self.global_transform.origin += - $CameraRod/MainCamera.global_transform.basis.z * speed
