d2d89f424901ede71aeba696172567c591c77678
shua <gpg@isthisa.email>
2024-11-17 04:51:50 +0100

    jumping grasshoppers

--- Bat/Entities/grasshopper.gd
+++ Bat/Entities/grasshopper.gd
@@ -1,9 +1,10 @@
-extends Node3D
+extends RigidBody3D
 
 signal chirp(pos: Vector3);
 
 var scared = false;
 var alive = true;
+var sleep_contact_point: Vector3;
 
 func _ready() -> void:
 	$Timer.start()
@@ -12,11 +13,18 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_timer_timeout():
+	if self.sleeping == true && randf() > 0.0:
+		var jump_dir = self.basis.y
+		if sleep_contact_point != null:
+			jump_dir = (self.global_position - sleep_contact_point).normalized()
+		self.apply_impulse(jump_dir * 5.0)
+	$Timer.start()
 	chirp.emit(self.position)
-	
+
 func unalive():
-	self.freeze = false;
+	self.sleeping = false;
 	self.alive = false
+	$Timer.stop()
 	
 func set_scared(s: bool):
 	if s:
@@ -24,3 +32,9 @@ func set_scared(s: bool):
 	else:
 		$Timer.start();
 	scared = s;
+
+func _on_body_entered(body: Node) -> void:
+	if self.alive && body is StaticBody3D:
+		var state: PhysicsDirectBodyState3D = PhysicsServer3D.body_get_direct_state(self.get_rid())
+		sleep_contact_point = state.get_contact_collider_position(0)
+		self.sleeping = true
